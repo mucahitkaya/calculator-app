ca6b49e2fe6ab5e157427cc5945bed924349e086
mücahit kaya <mucahit.kayaeee@hotmail.com>
2022-06-27 23:40:21 +0300

    last

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import theme from "./assets/theme1.jpg";
 
 function App() {
   return (
     <div className="App">
       <h1>Hellouu</h1>
+      <img src={theme} alt="as" />
     </div>
   );
 }
