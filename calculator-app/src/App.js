2c45958e9cfc30d4c6253c6c47a8046cb87e7ede
mücahit kaya <mucahit.kayaeee@hotmail.com>
2022-07-04 12:02:46 +0300

    this project will finish today

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -5,25 +5,37 @@ import Screen from "./screen/Screen";
 import ThemeBar from "./themebar/ThemeBar";
 
 function App() {
-  const body = document.getElementsByClassName("App");
+
   const darkTheme = "dark";
   const lightTheme = "light";
 
   const [themeStatus, setThemeStatus] = useState(true);
+  const [truthy,setTruthy]=useState(false);
+  const [sign,setSign]=useState("");
   const [value, setValue] = useState("0");
+  const [secondvalue, setSecondValue] = useState("0");
 
   function handleClick(e) {
+
+    //THEME--
     if (e.name === "theme") {
       if (e.value === "0") setThemeStatus(false);
       else setThemeStatus(true);
-    } else if (e.type === "submit") {
-      console.log(value);
-      setValue((aha) => aha + `${e.textContent}`);
+    } 
+    //KEYBOARD--
+    else if (e.type === "submit") {
+      //check for if there is not any input number but there is calculation sign input    
+      if(value==="0"&&isNaN(e.textContent))alert("Please enter number first!");
+      
     }
+  
   }
+ 
+  //  setValue((prevValue) => prevValue + `${clickedValue }`);
+    
+  
 
-  function adds(number) {}
-
+  
   return (
     <div className={`App ${themeStatus ? lightTheme : darkTheme}`}>
       <ThemeBar
